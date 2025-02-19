b678e3b2f445693e871b5c40ff330f6efb18ba9f
HaiderMalikk <haidermalik662@gmail.com>
2025-02-19 18:35:04 -0500

    Apis for MercurAI setup

---
+++ main.py
@@ -0,0 +1,13 @@
+# simple flask test app
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
+    
+# Run the app
+# python3 main.py
